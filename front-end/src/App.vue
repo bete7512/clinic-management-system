ddb04cc88ee3901ad51be16a7c02a8feb8661df2
bete7512 <betekbebe@gmail.com>
2022-09-19 16:09:49 +0300

    third commit

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,27 +1,11 @@
 <template>
-  <div class=" flex justify-center text-center text-red-500">
-    <div>
-      hello
-    </div>
+  <div class="text-red-500">
+    hello
   </div>
-  
 </template>
-<script setup>
-
-
-
+<script setup lang="ts">
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
